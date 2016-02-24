55478e83bcf56974046cf9da726ffab32f34f472
Nicolas Almagro Tonne <nixatto@gmail.com>
2016-02-24 16:01:34 +0100

    wat

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('vitensenteretKartCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('fargelSCtrl', function($scope) {
+
+})
+    
