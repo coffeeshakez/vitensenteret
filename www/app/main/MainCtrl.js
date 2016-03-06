7c55787e8d4c3d87f739bd8fe8ac95847e43d3c0
Nicolas Almagro Tonne <nixatto@gmail.com>
2016-03-06 17:47:08 +0100

    Arms too

--- www/app/main/MainCtrl.js
+++ www/app/main/MainCtrl.js
@@ -2,11 +2,19 @@ angular.module('app.main')
 .controller('MainCtrl', function($scope, $stateParams) {
 
     $scope.parts = [
-        {name: "Hode", type: "head", variants: [1, 2, 3], variant: 3, collected: true},
+        {name: "Hode", type: "head", variants: [1, 2, 3], variant: 3, collected: false},
         {name: "Armer", type: "arms", variants: [1, 2, 3], variant: 1, collected: false},
-        {name: "Bein", type: "legs", variants: [1, 2, 3, 4], variant: 1, collected: true},
+        {name: "Bein", type: "legs", variants: [1, 2, 3, 4], variant: 1, collected: false},
         {name: "Overkropp", type: "body", variants: [1, 2], variant: 2, collected: true},
     ];
+
+    $scope.collectedPartsCount = function(){
+        var count = 0;
+        angular.forEach($scope.parts, function(part){
+            count += part.collected ? 1 : 0;
+        });
+        return count; 
+    }
     
     $scope.partClasses = function(part){
         var collected = part.collected ? 'part-collected' : 'part-not-collected';
@@ -22,5 +30,9 @@ angular.module('app.main')
         else{
             part.variant += 1;
         }
+    }
+
+    $scope.partToggle = function(part){
+        part.collected ^= true;
     }
 });
