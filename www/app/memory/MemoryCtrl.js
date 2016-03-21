68b6efc7241876af26f0c02cccbe6c312301b1e9
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-03-21 18:25:38 +0100

    Memory game as good as done.

--- www/app/memory/MemoryCtrl.js
+++ www/app/memory/MemoryCtrl.js
@@ -1,12 +1,18 @@
 angular.module('app.memory')
-.controller('MemoryCtrl', function($scope, $stateParams) {
-  var numberOfWins = 0;
+.controller('MemoryCtrl', function($scope, $stateParams, $ionicPopup) {
+
   var greenButton = document.getElementById("memorygreen");
   var pinkButton = document.getElementById("memorypink");
   var yellowButton = document.getElementById("memoryyellow");
   var orangeButton = document.getElementById("memoryorange");
   var startButton = document.getElementById("startButton");
+
+  var numberOfWins = 0;
+  var clickNumber = 0;
   var gameList = [];
+  var clicked = [];
+
+
   function initGame(number){
     var game = [];
     for(i = 0; i<number; i++){
@@ -14,101 +20,120 @@ angular.module('app.memory')
     }
     return game;
   }
+
   function blink(button, time){
     button.style.opacity = "1";
     setTimeout(function(){
       button.style.opacity = "0.5";
     }, time)
   }
-  function checkIfWon(click, game){
-    console.log("y");
-    var numbs = 0;
-    for(i=0;i<game.length;i++){
-      if (click[i] === game[i]){
-        numbs+=1;
-      }
-    }
-    if(numbs === game.length){
-      console.log("hdef")
-    }
-  };
-  function playGame() {
-    console.log("hello");
-    var clicked = [];
+
+  function activateButtons(){
+    clicked = [];
     pinkButton.onclick = function () {
       blink(pinkButton, 300);
       clicked.push(1);
-      console.log(clicked);
-      if(clicked.length == gameList.length && checkIfWon(clicked, gameList)){
-        numberOfWins+=1
-      }
+      checkIfWon(clicked, gameList);
+      clickNumber+=1;
     };
     greenButton.onclick = function () {
       blink(greenButton, 300);
       clicked.push(2);
-      console.log(clicked);
-      if(clicked.length == gameList.length && checkIfWon(clicked, gameList)){
-        numberOfWins+=1
-      }
+      checkIfWon(clicked, gameList);
+      clickNumber+=1;
+
     };
     orangeButton.onclick = function () {
       blink(orangeButton, 300);
       clicked.push(3);
-      console.log(clicked);
-      if(clicked.length == gameList.length && checkIfWon(clicked, gameList)){
-        numberOfWins+=1
-      }
+      checkIfWon(clicked, gameList);
+      clickNumber+=1;
+
     };
     yellowButton.onclick = function () {
       blink(yellowButton, 300);
       clicked.push(4);
-      console.log(clicked);
-      if(clicked.length == gameList.length && checkIfWon(clicked, gameList)){
-        numberOfWins+=1
-      }
+      checkIfWon(clicked, gameList);
+      clickNumber+=1;
+
     };
 
   }
 
+  function deactivateButtons(){
+    yellowButton.onclick = null;
+    orangeButton.onclick = null;
+    pinkButton.onclick = null;
+    greenButton.onclick = null;
+  }
+
+  function gameWon(){
+    clickNumber = 0;
+    numberOfWins+=1;
+    startButton.style.display = "block"
+    deactivateButtons();
+    $ionicPopup.show({
+      title: "Du vant spill " + numberOfWins,
+      scope: $scope,
+      buttons: [
+        { text: "<b>Neste spill</b>",
+          type: "button-positive"}
+      ]
+    })
+
+  }
+
+  function checkIfWon(click, game){
+    if(!(click[clickNumber] === game[clickNumber])){
+      clickNumber = 0;
+      $ionicPopup.show({
+        title: 'Du tapte',
+        scope: $scope,
+        buttons: [
+          { text: '<b>Start på nytt</b>',
+            type: 'button-positive',
+            onTap: function() {
+              startButton.style.display = "block"
+            }
+          }
+        ]
+      });
+    }
+    if(click.length === game.length) {
+      gameWon();
+    }
+  }
+
+
 
 
   $scope.startGame = function(){
-    gameList = initGame((numberOfWins+1)*4);
-    console.log(typeof (gameList[0]));
+    clickNumber = 0;
+    gameList = [];
+    clicked = [];
+    gameList = initGame((numberOfWins+1)*3);
     startButton.style.display = "none";
-
     var i = 0;
+    console.log(gameList);
+    console.log(clicked);
     var opa = setInterval(function(){
       console.log(gameList[i])
       if(i > gameList.length){
-        playGame();
-        startButton.style.display = "block";
+        activateButtons();
         clearInterval(opa);
       }
       else {
         if(gameList[i] === 1 ){
-          pinkButton.style.opacity = "1";
-          setTimeout(function(){
-            pinkButton.style.opacity = "0.5";
-          }, 500)
+          blink(pinkButton, 500)
         }
         else if(gameList[i] == 2){
-          greenButton.style.opacity = "1";
-          setTimeout(function(){
-            greenButton.style.opacity = "0.5";
-          }, 500)
+          blink(greenButton, 500)
         }
         else if(gameList[i] === 3){
-          orangeButton.style.opacity = "1";
-          setTimeout(function(){
-            orangeButton.style.opacity = "0.5";
-          }, 500)
+          blink(orangeButton, 500)
         }
         else if(gameList[i] === 4){
-          yellowButton.style.opacity = "1";
-          setTimeout(function(){
-            yellowButton.style.opacity = "0.5";
-          }, 500)
+          blink(yellowButton, 500)
         }
         i+=1;
       }
