188d74222eae7caeb2b2ae1ee0028e2bd9f10f3e
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-04-04 12:13:41 +0200

    farts

--- www/app/reward/RewardCtrl.js
+++ www/app/reward/RewardCtrl.js
@@ -1,4 +1,8 @@
 angular.module('app.reward')
 .controller('RewardCtrl', function($scope, $stateParams) {
+  $scope.onInitialize = function(){
+    document.getElementById("robotPart").innerHTML = "robotarmer";
+    document.getElementById("gameFinished").innerHTML = "minnespillet";
+  }
 
 });
