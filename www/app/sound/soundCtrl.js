aeb8ff4f8477066cc08b21f59efb4f81333cfdd2
kristian svoren <kristian svoren>
2016-04-09 14:13:20 +0200

    ting begynner å ta form. Bildene funker. skal få på plass form

--- www/app/sound/soundCtrl.js
+++ www/app/sound/soundCtrl.js
@@ -4,10 +4,29 @@ angular.module('app.sound')
     // var src = "../../sound/Ready-Sangen.mp3";
     // var media = new Audio(src);
     var sangen = document.getElementById("song");
+    var viewInsert = document.getElementById("soundView");
+    var taskNumber = 0;
 
     $scope.playImitate = function(){
 
 		sangen.currentTime = 0;
         sangen.play();
+   };
+
+    $scope.initSound = function () {
+        insertTriangle();
+    };
+    
+    $scope.soundCheckCorrect = function () {
+        insertSquare();
+    }
+
+    function insertTriangle(){
+       viewInsert.innerHTML = triangle;
+      //document.getElementById("soundTestCorrect").onClick = insertSquare();
    }
+
+    function insertSquare(){
+        viewInsert.innerHTML = square;
+    }
 });
---
+++ www/app/sound/soundViews.js
@@ -0,0 +1,68 @@
+var triangle =`<img src="../../img/triangle.svg" class = "soundPhoto">
+
+    <select id="triangleOne" class="numberSelector">
+        <option selected>1</option>
+        <option >2</option>
+        <option >3</option>
+        <option >4</option>
+        <option >5</option>
+    </select>
+
+    <select id="triangleTwo" class="numberSelector">
+        <option >1</option>
+        <option selected>2</option>
+        <option >3</option>
+        <option >4</option>
+        <option >5</option>
+
+        
+    </select>
+
+    <select id="triangleThree" class="numberSelector">
+        <option >1</option>
+        <option >2</option>
+        <option selected>3</option>
+        <option >4</option>
+        <option >5</option>
+    </select>`;
+
+
+var square = `<img src="../../img/square.svg" class = "soundPhoto">
+
+   	<select id="squareOne" class="numberSelector">
+        <option selected>1</option>
+        <option >2</option>
+        <option >3</option>
+        <option >4</option>
+        <option >5</option>
+    </select>
+
+    <select id="squareTwo" class="numberSelector">
+        <option >1</option>
+        <option selected>2</option>
+        <option >3</option>
+        <option >4</option>
+        <option >5</option>
+        
+        
+    </select>
+
+    <select id="squareThree" class="numberSelector">
+        <option >1</option>
+        <option >2</option>
+        <option selected>3</option>
+        <option >4</option>
+        <option >5</option>
+    </select>
+    
+     <select id="squareFour" class="numberSelector">
+        <option >1</option>
+        <option >2</option>
+        <option selected>3</option>
+        <option >4</option>
+        <option >5</option>
+    </select>`;
+    `;
+
+
+   	`;
