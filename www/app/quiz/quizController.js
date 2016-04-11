dda8baf1cf71df8e7afef311c59ead68662e5552
coff33sh4k3 <davbakke@gmail.com>
2016-04-11 12:54:28 +0200

    Har fikset knapper og fonter, og fine ting.

--- www/app/quiz/quizController.js
+++ www/app/quiz/quizController.js
@@ -1,10 +1,9 @@
 angular.module('app.quiz')
-.controller('quizController', function($scope, $stateParams) {
-
+.controller('quizController', function($scope, $stateParams, $ionicPopup) {
 
       $scope.questions = [
         {
-          question: "Hvem oppfant revolveren",
+          question: "Hvem oppfant revolveren?",
           alternatives: ["Elisha H. Collier", "John Evans", "Samuel Colt", "George W. Bush"],
           correct: 0,
         },
@@ -14,6 +13,7 @@ angular.module('app.quiz')
           alternatives: ["37", "25", "19", "1000"],
           correct: 2,
         },
+
         {
           question: "Hvor mange kopper sukker må du ha med deg på månetur?",
           alternatives: ["10", "20", "50", "100,2"],
@@ -21,14 +21,16 @@ angular.module('app.quiz')
         },
 
         {
-          question: "tiss, bæsj promp?",
-          alternatives: ["promp", "bæsj", "tiss"],
-          correct: 1,
+         question: "test",
+          alternatives: ["fem", "ti", "tretti", "tjuefem"],
+          correct: 3,
         },
       ];
 
+  $scope.totalQ = $scope.questions.length;
+  $scope.qLeft = $scope.questions;
   $scope.qNum = 0;
-  $scope.ask = $scope.questions[$scope.qNum];
+  $scope.ask = $scope.qLeft[$scope.qNum];
   $scope.data = {
     clientSide: 'ng'
   };
@@ -38,15 +40,51 @@ angular.module('app.quiz')
     if(answer == $scope.ask.alternatives[$scope.ask.correct])
     {
       console.log("Riktig");
+      var pop = preparePopup("Riktig", "Hurraa, du svarte riktig")
+      var myPopup = $ionicPopup.show(pop);
+    }
+    else{
+      var pop = preparePopup("Feil", "Dette var vel ikke helt riktig, vel?")
+      var myPopup = $ionicPopup.show(pop);
+    }
 
-      if($scope.qNum <= $scope.questions.length - 1)
+    myPopup.then(function(res) {
+         console.log('Tapped!', res);
+      });
+  }
+
+function preparePopup(title, subTitle)
+  {
+    var pop = 
+    {
+      title: title,
+      subTitle: subTitle,
+      scope: $scope,
+      buttons: 
+      [
+        { 
+          text: '<b>Neste spørsmål!</b>',
+          type: 'button-positive',
+          onTap: function(e){
+            askNextQuestion()
+          }
+        }
+      ]
+    }
+    return pop;
+  }
+function askNextQuestion(){
+
+  if($scope.qNum <= $scope.qLeft.length - 1)
       {
         $scope.qNum ++;
         $scope.ask = $scope.questions[$scope.qNum];
-      }  
-    }
-  }
-  
+      }
+      else
+      {
+          $scope.qNum = 0;
+          $scope.ask = $scope.qLeft[$scope.qNum]
+      }
+}
+
 });
-
-
