bce45f2010d5cf7f67ceb101d49e99c1dfc7ee6d
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-04-13 10:19:42 +0200

    fixed buttons blinking more than once after startgame

--- www/app/memory/MemoryCtrl.js
+++ www/app/memory/MemoryCtrl.js
@@ -1,22 +1,11 @@
 angular.module('app.memory')
-.controller('MemoryCtrl', function($scope, $ionicPopup, $state) {
-
-
-  var greenButton = document.getElementById("memorygreen");
-  var pinkButton = document.getElementById("memorypink");
-  var yellowButton = document.getElementById("memoryyellow");
-  var orangeButton = document.getElementById("memoryorange");
-  var startButton = document.getElementById("startButton");
-  var redoButton = document.getElementById("redoButton");
-  var hintButton = document.getElementById("hintButton");
+.controller('MemoryCtrl', function($scope, $ionicPopup, $state, $timeout, $interval) {
 
   var numberOfWins = 0;
   var clickNumber = 0;
   var gameList = [];
   var clicked = [];
   var numberOfLost = 0;
-
-  $scope.levelNumerator = "Nivå " + (numberOfWins+1) + "/3";
 
   var normalPopup = {
     title: "Du klarte dette nivået",
@@ -27,6 +16,7 @@ angular.module('app.memory')
         onTap: changeLevelNumerator}
     ]
   };
+
   var hasWonPopup = {
     title: "Du klarte det siste nivået og har dermed vunnet dette spillet",
     scope: $scope,
@@ -34,83 +24,116 @@ angular.module('app.memory')
       { text: "<b>Videre</b>",
         type: "button-positive",
         onTap: function(){
-            $state.go("index.reward", {"game": "minnespillet", "part": "venstre robotarm", "sprite": "sprite arm arm1"});
+          $state.go("index.reward", {"game": "minnespillet", "part": "venstre robotarm", "sprite": "sprite arms arms4"});
         }}
     ]
   };
+
+  var lostGamePopup = {
+    title: 'Du tapte',
+    scope: $scope,
+    buttons: [
+      { text: '<b>Start på nytt</b>',
+        type: 'button-positive',
+        onTap: function() {
+          $scope.gameLost = false;
+          numberOfLost+=1;
+        }
+      }
+    ]
+  };
+
+  $scope.levelNumerator = "Nivå " + (numberOfWins+1) + "/3";
+  $scope.gameStarted = false;
+  $scope.gameLost = true;
+
+  $scope.gameFailedThreeTimes = true;
+
+  $scope.clickMemoryButton;
+
+  $scope.buttons = [
+    {class:'pink', active: false},
+    {class:'green', active: false},
+    {class:'orange', active: false},
+    {class:'yellow', active: false}
+  ];
+
+  function blink(button, time){
+    button.active = true;
+    $timeout(function(){
+      button.active = false;
+    }, time)
+  }
+
+  function blinkCorrectButton(iterator){
+    switch(gameList[iterator]){
+      case "pink":
+        blink($scope.buttons[0], 500);
+        break;
+      case "green":
+        blink($scope.buttons[1], 500);
+        break;
+      case "orange":
+        blink($scope.buttons[2], 500);
+        break;
+      case "yellow":
+        blink($scope.buttons[3], 500);
+        break;
+    }
+
+  }
 
   function changeLevelNumerator(){
     $scope.levelNumerator = "Nivå " + (numberOfWins+1) + "/3";
   }
 
-
-
   function initGame(number){
     var game = [];
     for(i = 0; i<number; i++){
-      game.push(Math.floor(Math.random()*4)+1);
+      switch (Math.floor(Math.random()*4)+1){
+        case 1:
+          game.push("pink");
+          break;
+        case 2:
+          game.push("green");
+          break;
+        case 3:
+          game.push("orange");
+          break;
+        case 4:
+          game.push("yellow");
+          break;
+      }
+      if (i > 0 && game[i] == game[i-1]){
+        game.pop();
+        i--;
+      }
     }
     return game;
   }
 
 
 
-
-  function blink(button, time){
-    button.style.opacity = "1";
-    setTimeout(function(){
-      button.style.opacity = "0.5";
-    }, time)
-  }
-
   function activateButtons(){
     clicked = [];
-    pinkButton.onclick = function () {
-      blink(pinkButton, 300);
-      clicked.push(1);
+    $scope.clickMemoryButton = function(button){
+      blink(button, 300);
+      clicked.push(button.class);
       checkIfWon(clicked, gameList);
       clickNumber+=1;
     };
-    greenButton.onclick = function () {
-      blink(greenButton, 300);
-      clicked.push(2);
-      checkIfWon(clicked, gameList);
-      clickNumber+=1;
-
-    };
-    orangeButton.onclick = function () {
-      blink(orangeButton, 300);
-      clicked.push(3);
-      checkIfWon(clicked, gameList);
-      clickNumber+=1;
-
-    };
-    yellowButton.onclick = function () {
-      blink(yellowButton, 300);
-      clicked.push(4);
-      checkIfWon(clicked, gameList);
-      clickNumber+=1;
-
-    };
-
   }
 
   function deactivateButtons(){
-    yellowButton.onclick = null;
-    orangeButton.onclick = null;
-    pinkButton.onclick = null;
-    greenButton.onclick = null;
+    $scope.clickMemoryButton = null;
   }
-
-
-
 
   function gameWon(){
     clickNumber = 0;
     numberOfLost = 0;
     numberOfWins+=1;
-    startButton.style.display = "block";
-    hintButton.style.display = "none";
+    $scope.gameStarted = false;
+    $scope.gameFailedThreeTimes = true;
     deactivateButtons();
     if(numberOfWins === 3){
       $ionicPopup.show(hasWonPopup)
@@ -124,83 +147,50 @@ angular.module('app.memory')
     if(!(clicked[clickNumber] === gameList[clickNumber])){
       deactivateButtons();
       clickNumber = 0;
-      $ionicPopup.show({
-        title: 'Du tapte',
-        scope: $scope,
-        buttons: [
-          { text: '<b>Start på nytt</b>',
-            type: 'button-positive',
-            onTap: function() {
-              redoButton.style.display = "block"
-              numberOfLost+=1;
-            }
-          }
-        ]
-      });
+      $ionicPopup.show(lostGamePopup);
     }
-    if(clicked.length === gameList.length) {
+     else if(clicked.length === gameList.length) {
       gameWon();
     }
   }
 
   function runGame(){
     var i = 0;
-    var opacityChange = setInterval(function(){
-      if(i >= gameList.length){
-        activateButtons();
-        clearInterval(opacityChange);
-      }
-      else {
-        if(gameList[i] === 1 ){
-          blink(pinkButton, 500)
-        }
-        else if(gameList[i] == 2){
-          blink(greenButton, 500)
-        }
-        else if(gameList[i] === 3){
-          blink(orangeButton, 500)
-        }
-        else if(gameList[i] === 4){
-          blink(yellowButton, 500)
-        }
-        i+=1;
-      }
-    }, 1000)
+    var interval = $interval(
+        function(){
+          if(i >= gameList.length){
+            activateButtons();
+            $interval.cancel(interval);
+          }
+          else{
+            blinkCorrectButton(i);
+            i+=1;
+          }
+        }, 1000);
+
   }
 
   $scope.startGame = function(){
+    $scope.gameStarted = true;
     clickNumber = 0;
     clicked = [];
     gameList = initGame((numberOfWins+3));
-    startButton.style.display = "none";
     runGame();
   };
 
   $scope.redoGame = function(){
     clickNumber = 0;
     clicked = [];
-    redoButton.style.display = "none";
+    $scope.gameLost = true;
     runGame();
     if(numberOfLost >=3){
-      hintButton.style.display = "block"
+      $scope.gameFailedThreeTimes = false;
     }
 
   };
 
   $scope.getHint = function(){
-    if(gameList[clicked.length] === 1){
-      blink(pinkButton, 500);
-    }
-    else if(gameList[clicked.length] === 2){
-      blink(greenButton, 500);
-    }
-    else if(gameList[clicked.length] === 3){
-      blink(orangeButton, 500);
-    }
-    else if(gameList[clicked.length] === 4){
-      blink(yellowButton, 500);
-    }
-
+    blinkCorrectButton(clicked.length);
   }
 
 });
