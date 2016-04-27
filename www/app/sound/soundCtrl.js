396065b880ead934342962d78ad5d2c4aead5fba
Kristian <krissvor@gmail.com>
2016-04-27 13:03:26 +0200

    mer skalering og css-dritt

--- www/app/sound/soundCtrl.js
+++ www/app/sound/soundCtrl.js
@@ -56,226 +56,234 @@ pentaTwo
         sangen.play();
    };
 
+    //DETTE FUNKER IKKE
+    function setSelectors () {
+        console.log("meh");
+        // for (var i = 0; i < 3; i++) {
+            var myEl = document.getElementById("triangle1");
+            console.log(myEl);
+        // }
+    };
+
     $scope.initSound = function () {
         initNext();
-
-
-    $scope.soundCheckCorrect = function () {
-        if(taskNumber==1){
-           showPopup(checkTriangle()[0], checkTriangle()[1]);
-        }
-
-        else if(taskNumber==2){
-            showPopup(checkSquare()[0], checkSquare()[1]);
-        }
-        else if (taskNumber == 3){
-            showPopup(checkPentagon()[0], checkPentagon()[1]);
-        }
+        setSelectors();
     }
 
-    function initNext(){
-        if(taskNumber==0){
-            $scope.currentStage = $scope.stages["triangle"];
+
+        $scope.soundCheckCorrect = function () {
+            if (taskNumber == 1) {
+                showPopup(checkTriangle()[0], checkTriangle()[1]);
+            }
+
+            else if (taskNumber == 2) {
+                showPopup(checkSquare()[0], checkSquare()[1]);
+            }
+            else if (taskNumber == 3) {
+                showPopup(checkPentagon()[0], checkPentagon()[1]);
+            }
         }
-        else if(taskNumber==1){
-            $scope.currentStage = $scope.stages["square"];
+
+        function initNext() {
+            if (taskNumber == 0) {
+                $scope.currentStage = $scope.stages["triangle"];
+            }
+            else if (taskNumber == 1) {
+                $scope.currentStage = $scope.stages["square"];
+            }
+            else if (taskNumber == 2) {
+                $scope.currentStage = $scope.stages["penta"];
+            }
+
+            taskNumber++;
+
         }
-        else if(taskNumber==2){
-            $scope.currentStage = $scope.stages["penta"];
-        }
 
-        taskNumber++;
-
-    }
-
-    function showPopup(bool, correctArray){
+        function showPopup(bool, correctArray) {
 
 
+            $scope.data = {};
 
+            if (bool == true) {
+                if (taskNumber == 3) {
+                    var pop = {
+                        title: "RIKTIG!",
+                        subTitle: "Du svarte riktig!",
+                        scope: $scope,
+                        buttons: [
+                            {
+                                text: '<b>Ta i mot din premie!</b>',
+                                type: 'button-positive',
 
-        $scope.data = {};
+                                onTap: function () {
+                                    $rootScope.winGame("sound");
+                                    return;
+                                }
 
-        if (bool == true){
-            if(taskNumber==3) {
-                var pop = {
-                    title: "RIKTIG!",
-                    subTitle: "Du svarte riktig!",
-                    scope: $scope,
-                    buttons: [
-                        {
-                            text: '<b>Ta i mot din premie!</b>',
-                            type: 'button-positive',
-
-                            onTap: function () {
-                                $rootScope.winGame("sound");
-                                return;
                             }
+                        ]
+                    }
+                }
 
-                        }
-                    ]
+                else {
+                    var pop = {
+                        title: "RIKTIG!",
+                        subTitle: "Du svarte riktig!",
+                        scope: $scope,
+                        buttons: [
+                            {
+                                text: '<b>Neste oppgave</b>',
+                                type: 'button-positive',
+
+                                onTap: function () {
+                                    initNext();
+                                }
+
+                            }
+                        ]
+                    }
                 }
             }
 
             else {
+                var feedback1 = "Dette var ikke helt riktig, du hadde rør";
+                var feedback2 = " på posisjon";
+                var feedback3 = "";
+                var feedback4 = " riktig";
+
+                for (var i = correctArray.length - 1; i >= 0; i--) {
+                    if (feedback3.length == 2 && correctArray[i] == 1) {
+                        feedback3 = (i + 1) + " og " + feedback3;
+                        feedback1 += "ene ";
+                        feedback2 += "ene ";
+                    }
+                    else if (feedback3.length == 0 && correctArray[i] == 1) {
+                        feedback3 = " " + (i + 1) + feedback3;
+                    }
+
+                    else if (correctArray[i] == 1) {
+                        feedback3 = (i + 1) + ", " + feedback3;
+                    }
+
+                }
+
+                if (feedback3.length == 0) {
+                    feedback1 = "Dette var ikke riktig. Du har dessverre ingen rør på riktig posisjon."
+                    feedback2 = "";
+                    feedback3 = "";
+                    feedback4 = "";
+                }
+                else if (feedback3.length == 2) {
+                    feedback1 += "et "
+                }
                 var pop = {
-                    title: "RIKTIG!",
-                    subTitle: "Du svarte riktig!",
+                    title: "FEIL",
+                    subTitle: feedback1 + feedback2 + feedback3 + feedback4,
                     scope: $scope,
                     buttons: [
                         {
-                            text: '<b>Neste oppgave</b>',
+                            text: '<b>Prøv igjen</b>',
                             type: 'button-positive',
 
                             onTap: function () {
-                                initNext();
+
+
                             }
 
                         }
                     ]
                 }
-            }
-        }
-
-        else{
-            var feedback1 = "Dette var ikke helt riktig, du hadde rør";
-            var feedback2 = " på posisjon";
-            var feedback3 = "";
-            var feedback4 = " riktig";
-
-            for(var i=correctArray.length - 1; i>=0; i--){
-                if(feedback3.length == 2 && correctArray[i] == 1){
-                    feedback3 = (i+1) +  " og " + feedback3;
-                    feedback1+= "ene ";
-                    feedback2+= "ene ";
-                }
-                else if(feedback3.length == 0 && correctArray[i] == 1){
-                    feedback3 = " " + (i+1) + feedback3;
-                }
-
-                else if(correctArray[i] == 1) {
-                    feedback3 = (i + 1) + ", " + feedback3;
-                }
 
             }
 
-            if(feedback3.length == 0){
-                feedback1 = "Dette var ikke riktig. Du har dessverre ingen rør på riktig posisjon."
-                feedback2 = "";
-                feedback3 = "";
-                feedback4 = "";
-            }
-            else if(feedback3.length == 2){
-                feedback1 += "et "
-            }
-            var pop = {
-                title: "FEIL",
-                subTitle:  feedback1 +  feedback2 + feedback3 + feedback4,
-                scope: $scope,
-                buttons:[
-                {
-                    text: '<b>Prøv igjen</b>',
-                    type: 'button-positive',
+            var myPopup = $ionicPopup.show(pop);
 
-                    onTap: function(){
+            myPopup.then(function (res) {
+                console.log('Tapped!', res);
+            });
+        }
 
-
-                    }
-
-                    }
-                ]
+        function checkTriangle() {
+            var rettArray = [0, 0, 0, 0, 0];
+            var antallRett = 0;
+            console.log($scope);
+            if (document.getElementById("triangle-0").value == 1) {
+                rettArray[0] = 1;
+                antallRett++;
             }
 
+            if (document.getElementById("triangle-1").value == 2) {
+                rettArray[1] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("triangle-2").value == 3) {
+                rettArray[2] = 1
+                antallRett++;
+            }
+            if (antallRett == 3) {
+                return [true, rettArray];
+            }
+            return [false, rettArray];
+
         }
 
-        var myPopup = $ionicPopup.show(pop);
+        function checkSquare() {
+            var rettArray = [0, 0, 0, 0, 0];
+            var antallRett = 0;
+            if (document.getElementById("square-0").value == 1) {
+                rettArray[0] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("square-1").value == 2) {
+                rettArray[1] = 1;
+                antallRett++;
+            }
 
-        myPopup.then(function(res) {
-            console.log('Tapped!', res);
-        });
-    }
+            if (document.getElementById("square-2").value == 3) {
+                rettArray[2] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("square-3").value == 4) {
+                rettArray[3] = 1;
+                antallRett++;
+            }
+            if (antallRett == 4) {
+                return [true, rettArray]
+            }
 
-    function checkTriangle(){
-        var rettArray = [0,0,0,0,0];
-        var antallRett = 0;
-        console.log($scope);
-        if(document.getElementById("triangle-0").value == 1) {
-            rettArray[0] = 1;
-            antallRett++;
-        }
-
-        if(document.getElementById("triangle-1").value == 2) {
-            rettArray[1] = 1;
-            antallRett++;
-        }
-        if(document.getElementById("triangle-2").value == 3){
-            rettArray[2] = 1
-            antallRett++;
-        }
-        if (antallRett == 3){
-            return [true, rettArray];
-        }
-        return [false,rettArray];
-
-    }
-
-    function checkSquare(){
-        var rettArray = [0,0,0,0,0];
-        var antallRett =0;
-        if (document.getElementById("square-0").value == 1) {
-            rettArray[0] = 1;
-            antallRett++;
-        }
-        if (document.getElementById("square-1").value == 2) {
-            rettArray[1] = 1;
-            antallRett++;
+            return [false, rettArray];
         }
 
-        if (document.getElementById("square-2").value == 3) {
-            rettArray[2] = 1;
-            antallRett++;
-        }
-        if (document.getElementById("square-3").value == 4) {
-            rettArray[3] = 1;
-            antallRett++;
-        }
-        if(antallRett ==4){
-            return [true, rettArray]
-        }
-
-        return [false, rettArray];
-    }
-
-    function checkPentagon(){
-        var rettArray = [0,0,0,0,0];
-        var antallRett =0;
-        if(document.getElementById("penta-0").value == 1){
-            rettArray[0] = 1;
-            antallRett++;
-        }
-        if(document.getElementById("penta-1").value == 2){
-            rettArray[1] = 1;
-            antallRett++;
-        }
-        if(document.getElementById("penta-2").value == 3){
-            rettArray[2] = 1;
-            antallRett++;
-        }
-        if(document.getElementById("penta-3").value == 4){
-            rettArray[3] = 1;
-            antallRett++;
-        }
-        if (document.getElementById("penta-4").value = 5) {
-            rettArray[4] = 1;
-            antallRett++;
-        }
+        function checkPentagon() {
+            var rettArray = [0, 0, 0, 0, 0];
+            var antallRett = 0;
+            if (document.getElementById("penta-0").value == 1) {
+                rettArray[0] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("penta-1").value == 2) {
+                rettArray[1] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("penta-2").value == 3) {
+                rettArray[2] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("penta-3").value == 4) {
+                rettArray[3] = 1;
+                antallRett++;
+            }
+            if (document.getElementById("penta-4").value = 5) {
+                rettArray[4] = 1;
+                antallRett++;
+            }
             // HER MÅ DET HÅNDTERES OM SPILLET ER FERDIG!!
-        if(antallRett == 5){
-            return [true, rettArray]
+            if (antallRett == 5) {
+                return [true, rettArray]
+            }
+
+            return [false, rettArray];
         }
 
-        return [false, rettArray];
-    }
 
-
-    };
-});
+    });
