cedc60079d9543aef0ab1c8d5ae9248e9dc0505d
didrikpa <didrikpa@gmail.com>
2016-04-27 15:43:00 +0200

    hello kitty2

--- www/app/overview/OverviewCtrl.js
+++ www/app/overview/OverviewCtrl.js
@@ -1,5 +1,7 @@
 angular.module('app.overview')
-.controller('OverviewCtrl', function($scope, $rootScope, $state, $stateParams, localStorageService, $ionicPopup) {
+.controller('OverviewCtrl', function($scope, $rootScope, $state, $stateParams, localStorageService, $ionicPopup, $translate) {
+
+
 
     var minigamesLocal = localStorageService.get('minigames');
     var partsLocal = localStorageService.get('parts');
@@ -12,6 +14,18 @@ angular.module('app.overview')
         console.log("Stored language is: "+$rootScope.language);
     }
 
+    $translate(["OVERVIEW_QUIZ_BUTTON",
+    "OVERVIEW_ELEMENTS_BUTTON",
+    "OVERVIEW_COLOR_BUTTON",
+    "OVERVIEW_MELODY_BUTTON",
+    "OVERVIEW_WATER_BUTTON",
+    "OVERVIEW_SIMON_SAYS_BUTTON",
+    "OVERVIEW_SHORTEST_PATH_BUTTON",
+    "OVERVIEW_POPUP_START_BUTTON",
+    "OVERVIEW_POPUP_CANCEL_BUTTON"]).then(function(translations){
+        $scope.translations = translations;
+    });
+
     $scope.$watch('minigames', function () {
       localStorageService.set('minigames', $scope.minigames);
     }, true);
@@ -21,13 +35,13 @@ angular.module('app.overview')
     }, true);
 
     $rootScope.minigames = minigamesLocal || {
-        "quiz":      {name: "Quiz",           game: "quiz",      icon: "ion-help",              part: "head",   collected: false, story: "En liten Europa-måneboer lurer på hvordan Jordas historie er. Kan du hjelpe den?"},
-        "periodic":  {name: "Grunnstoffer",   game: "periodic",  icon: "ion-nuclear",           part: "body",   collected: false, story: "En gal vitenskapsmann prøver å lage en tidsmaskin, men vet ikke hvordan han skal lage ingrediensene. Kan du hjelpe?"},
-        "colors":    {name: "Fargelås",       game: "colors",    icon: "ion-lock-combination",  part: "head",   collected: false, story: "Du finner en låst kiste med en rar kombinasjonslås som har forskjellige farger. Klarer du å låse den opp?"},
-        "sound":     {name: "Melodispillet",  game: "sound",     icon: "ion-music-note",        part: "head",   collected: false, story: "En liten fugl har mistet moren sin. Kan du hjelpe den å svare på moren sin fuglesang?"},
-        "waterflow": {name: "Vannkobling",    game: "waterflow", icon: "ion-waterdrop",         part: "arms",   collected: false, story: "Det spruter vann i alle retninger ut av vannkraftverket. Kan du få rørene på plass så det genererer strøm igjen?"},
-        "memory":    {name: "Minnespillet",   game: "memory",    icon: "ion-load-b",            part: "arms",   collected: false, story: "Du møter på et glemskt orakel som har mistet hukommelsen. Kan du hjelpe det å huske kombinasjonen til orakel-skipet sitt?"},
-        "shortest":  {name: "Korteste veien", game: "shortest",  icon: "ion-map",               part: "legs",   collected: false, story: "Du møter på en handelsmann som skal reise landet rundt. Kan du hjelpe han finne den korteste veien?"},
+        "quiz":      {name: "OVERVIEW_QUIZ_BUTTON",           game: "quiz",      icon: "ion-help",              part: "head",   collected: false, story: "En liten Europa-måneboer lurer på hvordan Jordas historie er. Kan du hjelpe den?"},
+        "periodic":  {name: "OVERVIEW_ELEMENTS_BUTTON",   game: "periodic",  icon: "ion-nuclear",           part: "body",   collected: false, story: "En gal vitenskapsmann prøver å lage en tidsmaskin, men vet ikke hvordan han skal lage ingrediensene. Kan du hjelpe?"},
+        "colors":    {name: "OVERVIEW_COLOR_BUTTON",       game: "colors",    icon: "ion-lock-combination",  part: "head",   collected: false, story: "Du finner en låst kiste med en rar kombinasjonslås som har forskjellige farger. Klarer du å låse den opp?"},
+        "sound":     {name: "OVERVIEW_MELODY_BUTTON",  game: "sound",     icon: "ion-music-note",        part: "head",   collected: false, story: "En liten fugl har mistet moren sin. Kan du hjelpe den å svare på moren sin fuglesang?"},
+        "waterflow": {name: "OVERVIEW_WATER_BUTTON",    game: "waterflow", icon: "ion-waterdrop",         part: "arms",   collected: false, story: "Det spruter vann i alle retninger ut av vannkraftverket. Kan du få rørene på plass så det genererer strøm igjen?"},
+        "memory":    {name: "OVERVIEW_SIMON_SAYS_BUTTON",   game: "memory",    icon: "ion-load-b",            part: "arms",   collected: false, story: "Du møter på et glemskt orakel som har mistet hukommelsen. Kan du hjelpe det å huske kombinasjonen til orakel-skipet sitt?"},
+        "shortest":  {name: "OVERVIEW_SHORTEST_PATH_BUTTON", game: "shortest",  icon: "ion-map",               part: "legs",   collected: false, story: "Du møter på en handelsmann som skal reise landet rundt. Kan du hjelpe han finne den korteste veien?"},
 
     };
 
