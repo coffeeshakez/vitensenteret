7546278bae58030c6c75a5f380ae44bfca343a9c
didrikpa <didrikpa@gmail.com>
2016-05-29 17:26:03 +0200

    Kommenterte i memory-jsfilen

--- www/app/memory/MemoryCtrl.js
+++ www/app/memory/MemoryCtrl.js
@@ -1,12 +1,17 @@
+/*This is the controller for the memory game, also known as Simon says.*/
+
 angular.module('app.memory')
 .controller('MemoryCtrl', function($scope, $rootScope, $ionicPopup, $state, $timeout, $interval) {
 
-  var numberOfWins = 0;
-  var clickNumber = 0;
-  var gameList = [];
-  var clicked = [];
-  var numberOfLost = 0;
 
+
+  var numberOfWins = 0; // Number of levels that has been beaten
+  var clickNumber = 0; // Number of clicks by the user on the current level, after blinking is done
+  var gameList = []; // The list which contains what buttons that will be pushed and in what order.
+  var clicked = []; // The list which contains what buttons the user has clicked on the current level.
+  var numberOfLost = 0; // Number of times the user has lost the current level.
+
+  // The popup which appears when a level is finished
   var normalPopup = {
     title: "Du klarte dette nivået",
     scope: $scope,
@@ -17,6 +22,7 @@ angular.module('app.memory')
     ]
   };
 
+  //The popup which appears when all levels have been finished
   var hasWonPopup = {
     title: "Du klarte det siste nivået og har dermed vunnet dette spillet",
     scope: $scope,
@@ -29,6 +35,7 @@ angular.module('app.memory')
     ]
   };
 
+  // The popup which appers when a level has been lost.
   var lostGamePopup = {
     title: 'Du tapte',
     scope: $scope,
@@ -43,14 +50,15 @@ angular.module('app.memory')
     ]
   };
 
-  $scope.levelNumerator = "Nivå " + (numberOfWins+1) + "/3";
-  $scope.gameStarted = false;
-  $scope.gameLost = true;
+  $scope.levelNumerator = "Nivå " + (numberOfWins+1) + "/3"; // Displays how many levels that have been defeated in the view.
+  $scope.gameStarted = false; // If the level has started or not
+  $scope.gameLost = true; // If the level has been lost or not
 
-  $scope.gameFailedThreeTimes = true;
+  $scope.gameFailedThreeTimes = true; // If the level has been lost three times or more, or not. This is used to give the user the ability to get hints to finish the level.
 
   $scope.clickMemoryButton;
 
+  // The four buttons that blick and are to be pushed.
   $scope.buttons = [
     {class:'pink', active: false},
     {class:'green', active: false},
@@ -58,6 +66,7 @@ angular.module('app.memory')
     {class:'yellow', active: false}
   ];
 
+  //The blink function makes the button given as input blink for the time given as input. time is given as milliseconds.
   function blink(button, time){
     button.active = true;
     $timeout(function(){
@@ -65,6 +74,7 @@ angular.module('app.memory')
     }, time)
   }
 
+  // Used to make the correct button blink for 500 milliseconds
   function blinkCorrectButton(iterator){
     switch(gameList[iterator]){
       case "pink":
@@ -83,13 +93,15 @@ angular.module('app.memory')
 
   }
 
+  // Changes how many levels that has been defeated after a level victory.
   function changeLevelNumerator(){
     $scope.levelNumerator = "Nivå " + (numberOfWins+1) + "/3";
   }
 
+  // Initializes the game with "number" as number of times the buttons will blink. The number of times the button blinks is how difficult the level is.
   function initGame(number){
     var game = [];
-    for(i = 0; i<number; i++){
+    for(i = 0; i<number; i++){ // Fills the gamelist with buttonvalues.
       switch (Math.floor(Math.random()*4)+1){
         case 1:
           game.push("pink");
@@ -104,7 +116,7 @@ angular.module('app.memory')
           game.push("yellow");
           break;
       }
-      if (i > 0 && game[i] == game[i-1]){
+      if (i > 0 && game[i] == game[i-1]){ // Checks if the any one button is repeated twice.
         game.pop();
         i--;
       }
@@ -114,20 +126,23 @@ angular.module('app.memory')
 
 
 
+  // Activates the blinking buttons for clicking and checks if the level has been won.
   function activateButtons(){
     clicked = [];
-    $scope.clickMemoryButton = function(button){
-      blink(button, 300);
-      clicked.push(button.class);
-      checkIfWon(clicked, gameList);
+    $scope.clickMemoryButton = function(button){ // Everytime a button is clicked
+      blink(button, 300); //the button blinks
+      clicked.push(button.class); // and is pushed into the clicked list
+      checkIfWon(clicked, gameList); //Check if the level has been won
       clickNumber+=1;
     };
   }
 
+  //Deactivates the buttons for clicking.
   function deactivateButtons(){
     $scope.clickMemoryButton = null;
   }
 
+  // When a level is won.
   function gameWon(){
     clickNumber = 0;
     numberOfLost = 0;
@@ -135,7 +150,7 @@ angular.module('app.memory')
     $scope.gameStarted = false;
     $scope.gameFailedThreeTimes = true;
     deactivateButtons();
-    if(numberOfWins === 3){
+    if(numberOfWins === 3){ // show popup
       $ionicPopup.show(hasWonPopup)
     }
     else{
@@ -143,21 +158,23 @@ angular.module('app.memory')
     }
   }
 
+  // Check if the level has been won.
   function checkIfWon(){
-    if(!(clicked[clickNumber] === gameList[clickNumber])){
+    if(!(clicked[clickNumber] === gameList[clickNumber])){ //Check whether the clicked button is the same as the button that blinked.
       deactivateButtons();
       clickNumber = 0;
-      $ionicPopup.show(lostGamePopup);
+      $ionicPopup.show(lostGamePopup); // show popup
     }
      else if(clicked.length === gameList.length) {
       gameWon();
     }
   }
 
+  //Starts the blinking of buttons when a game is started.
   function runGame(){
     var i = 0;
     var interval = $interval(
-        function(){
+        function(){ // make the buttons blink with 500 milliseconds between every blink.
           if(i >= gameList.length){
             activateButtons();
             $interval.cancel(interval);
@@ -170,14 +187,16 @@ angular.module('app.memory')
 
   }
 
+  //Function for starting a level.
   $scope.startGame = function(){
-    $scope.gameStarted = true;
+    $scope.gameStarted = true; //game started is true
     clickNumber = 0;
     clicked = [];
-    gameList = initGame((numberOfWins+3));
-    runGame();
+    gameList = initGame((numberOfWins+3)); //initialize the gamelist
+    runGame(); //make the buttons blink
   };
 
+  //If the game was lost, this makes the same level start over again, with  the same buttons blinking in the same order.
   $scope.redoGame = function(){
     clickNumber = 0;
     clicked = [];
@@ -189,6 +208,7 @@ angular.module('app.memory')
 
   };
 
+  //Function for showing a hint. Only possible after failing a level 3 times or more.
   $scope.getHint = function(){
     blinkCorrectButton(clicked.length);
   }
